065e09fe634de5075176df699400b17b60149928
dormon <imilet@fit.vutbr.cz>
2024-03-13 19:19:59 +0100

    getprocadd

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 int main(int argc,char*argv[]){
   fprintf(stderr,"Ahoj svete\n");
 
-  auto window = SDL_CreateWindow("izg06",0,0,1024,768,0);
+  auto window = SDL_CreateWindow("izg06",0,0,1024,768,SDL_WINDOW_OPENGL);
+
+  auto context = SDL_GL_CreateContext(window);
+
+  using GLCLEARCOLOR = void(*)(float,float,float,float);
+  using GLCLEAR      = void(*)(int);
+
+#define GL_COLOR_BUFFER_BIT			0x00004000
+  GLCLEARCOLOR glClearColor = (GLCLEARCOLOR)SDL_GL_GetProcAddress("glClearColor");
+  GLCLEAR      glClear      = (GLCLEAR     )SDL_GL_GetProcAddress("glClear"     );
 
   bool running = true;
   while(running){ // main loop
@@ -14,8 +23,16 @@ int main(int argc,char*argv[]){
         
     }
 
+    //draw
+    
+    glClearColor(0,0,1,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    SDL_GL_SwapWindow(window);
   }
 
+  SDL_GL_DeleteContext(context);
   SDL_DestroyWindow(window);
 
   return 0;
